d76de67fc56c3a8f1eab3220dcde1f09a3897ba8
pzohaycuoi <pzohaycuoi@gmail.com>
2022-01-07 04:16:03 +0700

    update New-NamBulKAdUser

--- bin/New-NamBulkAdUser.ps1
+++ bin/New-NamBulkAdUser.ps1
@@ -20,5 +20,49 @@ function New-NamBulkAdUser {
   $resultFile = New-Item -Path $resultPath.FilePath -Name "log-$(get-date -Format ddMMyyyy-hhmmss).log" -Force  
 
   # Check if csv path exist
-  $checkIfFileExist = Test-F
+  $checkIfFileExist = Test-FileExist -FilePath $FilePath -FileExtension ".csv"
+  
+  if ($checkIfFileExist.Result -eq $true) {
+    New-Log -Level "INFO" -Message $checkIfFileExist.Log -LogFile $logFile
+
+    # check if input csv has required headers
+    $csvReqheader = "$($scriptDir)\..\data\input-required-header-csv.txt"
+    $checkCsvReqHeader = Get-CsvRequiredHeader -FilePath $FilePath -RequiredHeaderFile $csvReqheader
+
+    if ($checkCsvReqHeader.Result -eq $true) {
+      New-Log -Level "INFO" -Message $checkCsvReqHeader.Log -LogFile $logFile
+
+      # import and loop through csv file
+      $csvData = Import-Csv -Path $FilePath
+
+      foreach ($row in $csvData) {
+        $data = [PSCustomObject]@{
+          FirstName = $row.FirstName
+          LastName = $row.LastName
+          Title = $row.Title
+          Department = $row.Department
+          Manager = $row.Manager
+          Location = $row.Location
+          EmployeeNumber = $row.EmployeeNumber
+        }
+        $createUser = New-NamAdUser -FirstName $data.FirstName `
+          -LastName $data.LastName `
+          -Title $data.Title `
+          -Department $data.Department `
+          -Manager $data.Manager `
+          -Location $data.Location `
+          -EmployeeNumber $data.EmployeeNumber `
+          -LogFile $logFile
+        Write-Host $createUser
+      }
+    }
+    else {
+      New-Log -Level "ERROR" -Message $checkCsvReqHeader.Log -LogFile $logFile
+      return $checkCsvReqHeader
+    }
+  }
+  else {
+     New-Log -Level "ERROR" -Message $checkIfFileExist.Log -LogFile $logFile
+     return $checkIfFileExist
+  }
 }
